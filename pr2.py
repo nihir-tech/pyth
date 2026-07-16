da16de588eaf9f8a680d47c444caee55bf0f6315
Nihir Sartanpara <sartanparanihir@gmail.com>
2026-07-16 20:54:30 +0530

    Create pr2.py

---
+++ pr2.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+by = int(input("Enter your birth year: "))
+
+cy = 2026
+age = cy - by
+
+if age >= 60:
+    print("Is Senior Citizen")
+else:
+    print("Not a Senior Citizen")
