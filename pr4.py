d24cdd525ffce0d56ec07bb850b0173eb05b1147
Nihir Sartanpara <sartanparanihir@gmail.com>
2026-07-16 20:57:34 +0530

    Implement binomial coefficient calculation

---
+++ pr4.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact *= i
+    return fact
+
+n = int(input("Enter n: "))
+r = int(input("Enter r: "))
+
+ncr = factorial(n) // (factorial(r) * factorial(n - r))
+
+print(f"Binomial Coefficient {n}C{r} = {ncr}")
