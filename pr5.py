52c8f3a2846ea82b24ae55ea7cb478b8aba17540
Nihir Sartanpara <sartanparanihir@gmail.com>
2026-07-31 07:55:23 +0530

    Create pr5.py

---
+++ pr5.py
@@ -0,0 +1,14 @@
+import math
+n = int(input("Enter the number of elements: "))
+numbers = []
+print("Enter the numbers:")
+for i in range(n):
+    num = float(input())
+    numbers.append(num)
+mean = sum(numbers) / n
+variance = sum((x - mean) ** 2 for x in numbers) / n
+std_deviation = math.sqrt(variance)
+print("\nResults:")
+print("Mean =", mean)
+print("Variance =", variance)
+print("Standard Deviation =", std_deviation)
